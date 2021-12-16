1610b1aca66c6f8dee2a8bda18e96d711ac96a8e
Ahmed Hakeem <hakeemkoo@gmail.com>
2021-12-16 12:34:28 -0500

    first commit and the header finshed

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className=" ">
+      <Head>
+        <title>INSTAGRAM Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+      <Header/>
+      {/* Feed */}
+
+      {/* Modal */}
+
+
+    </div>
+  )
+}
